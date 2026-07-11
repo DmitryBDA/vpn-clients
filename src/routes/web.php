e77d075c8ffbe7f03849d4df663839dc575e0249
Belousov Dmitry <da_belousov@list.ru>
2026-07-11 10:04:08 +0300

    add admin lte

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Admin\Main\IndexController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
+    Route::get('/', [IndexController::class, 'index'])->name('admin.index');
 });
