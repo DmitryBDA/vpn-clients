07486d653555a7c66430afde3b6900fedf84de06
Belousov Dmitry <da_belousov@list.ru>
2026-07-14 03:44:53 +0300

    add admin role and user role

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +17,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $users = User::query()->where('name', '!=', 'admin')->get();
+
+        $userRole = Role::findOrCreate('user');
+
+        foreach ($users as $user) {
+            $user->assignRole($userRole);
+        }
+
+        $user = User::query()->where('name', 'admin')->first();
+
+        if (is_null($user)) {
+
+            $user =  User::factory()->create([
+                'name' => 'admin',
+                'email' => 'admin@admin.com',
+                'password' =>  Hash::make('admin@admin.com'),
+            ]);
+
+            $role = Role::findOrCreate('admin');
+
+            $user->assignRole($role);
+        }
+
+
     }
 }
